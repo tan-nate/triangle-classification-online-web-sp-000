90c6ac8bb152b94c6311995f55086b13a21ad00e
Nate Tan <nktan93@gmail.com>
2019-04-11 19:54:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,17 +9,9 @@ class Triangle
   
   def kind
     if (side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1
-      #begin 
-        raise TriangleError
-      # rescue TriangleError => error
-      #   puts error.message
-      # end
+      raise TriangleError
     elsif side1 <= 0 || side2 <= 0 || side3 <= 0
-      #begin 
-        raise TriangleError
-      # rescue TriangleError => error
-      #   puts error.message
-      # end
+      raise TriangleError
     else
       if side1 == side2 && side2 == side3
         :equilateral
@@ -32,11 +24,5 @@ class Triangle
   end
   
   class TriangleError < StandardError
-    # def message 
-    #   "The sum of two sides of a triangle must be greater than the third side. All sides must be greater than 0."
-    # end
   end
 end
-
-#triangle = Triangle.new(1, 0, 3)
-#puts triangle.kind
